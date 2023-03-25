2ad9b6282ee2f17750d23522d7648c32b6e03327
shittu adewale <sheriffhardewale@gmail.com>
2023-03-25 01:44:18 +0100

    removed padding

--- components/dashboard/SimpleNav.jsx
+++ components/dashboard/SimpleNav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SimpleNav = ({title, user}) => {
   return (
-    <div className='flex justify-between pt-4 px-8'>
+    <div className='flex  justify-between pt-4 px-6'>
           <h3 className='font-semibold capitalize normal-text text-xl'>{title}</h3>
           <div className='flex items-center gap-2'>
             <div className='w-12 h-12 rounded-full bg-brand-blue'></div>
