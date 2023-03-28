96b26e2075302f86bbe0dd3b5f6cf17473dd60c5
shittu adewale <sheriffhardewale@gmail.com>
2023-03-28 07:38:54 +0100

    added popover

--- components/dashboard/SimpleNav.jsx
+++ components/dashboard/SimpleNav.jsx
@@ -3,20 +3,178 @@ import { MenuIcon } from "@/public/svgs/MenuIcon";
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import MobileSidebar from "./mobile-componets/MobileSidebar";
+import { Popover, Transition } from "@headlessui/react";
 
+import { Fragment } from "react";
+import {HiOutlineChevronDown} from "react-icons/hi"
+
+const solutions = [
+  {
+    name: "My Profile",
+    description: "Measure actions your users take",
+    href: "##",
+    icon: IconOne,
+  },
+  {
+    name: "Logout",
+    description: "Create your own targeted content",
+    href: "##",
+    icon: IconTwo,
+  },
+ 
+];
 const SimpleNav = ({ title, user }) => {
   const { showMenu, setShowMenu } = useDashboardProvider();
   //  const [showMenu, setShowMenu] = useState(false)
   return (
     <div className="relative">
-      <div className="md:px-9 px-6">
-
-     
+      <div className="md:px-9 py-4 px-6">
+        <div className="flex justify-between items-center">
+          <h3 className="font-semibold hidden md:inline-block capitalize normal-text text-xl">
+            {title}
+          </h3>
+          <div className="flex  items-center gap-1 md:gap-2">
+            <div className="md:w-12 w-8 h-8 md:h-12 rounded-full bg-brand-blue"></div>
+            <div className="">
+              <Popover className="relative">
+                {({ open }) => (
+                  <>
+                    <Popover.Button
+                      className={`
+                ${open ? "" : "text-opacity-90"}
+                group inline-flex items-center rounded-md px-3 py-2 text-base font-medium  hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+                    >
+                      <span>John Okafor</span>
+                      <HiOutlineChevronDown
+                        className={`${open ? "" : "text-opacity-70"}
+                   text-black text-xl transition duration-150 ease-in-out group-hover:text-opacity-80`}
+                        aria-hidden="true"
+                      />
+                    </Popover.Button>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-200"
+                      enterFrom="opacity-0 translate-y-1"
+                      enterTo="opacity-100 translate-y-0"
+                      leave="transition ease-in duration-150"
+                      leaveFrom="opacity-100 translate-y-0"
+                      leaveTo="opacity-0 translate-y-1"
+                    >
+                      <Popover.Panel className="absolute left-1/4 z-10 mt-3 max-w-sm -translate-x-1/2 transform px-4 sm:px-0 ">
+                        <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5">
+                          <div className="relative bg-white p-5">
+                            {solutions.map((item) => (
+                              <a
+                                key={item.name}
+                                href={item.href}
+                                className=" flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500 focus-visible:ring-opacity-50"
+                              >
+                                {/* <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
+                                  <item.icon aria-hidden="true" />
+                                </div> */}
+                                <div className="">
+                                  <p className="text-md font-medium text-gray-900">
+                                    {item.name}
+                                  </p>
+                                  
+                                </div>
+                              </a>
+                            ))}
+                          </div>
+                       
+                        </div>
+                      </Popover.Panel>
+                    </Transition>
+                  </>
+                )}
+              </Popover>
+            </div>
+          </div>
+        </div>
       </div>
 
-    {showMenu && <MobileSidebar />}
+      {showMenu && <MobileSidebar />}
     </div>
   );
 };
 
 export default SimpleNav;
+
+function IconOne() {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+      <path
+        d="M24 11L35.2583 17.5V30.5L24 37L12.7417 30.5V17.5L24 11Z"
+        stroke="#FB923C"
+        strokeWidth="2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M16.7417 19.8094V28.1906L24 32.3812L31.2584 28.1906V19.8094L24 15.6188L16.7417 19.8094Z"
+        stroke="#FDBA74"
+        strokeWidth="2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M20.7417 22.1196V25.882L24 27.7632L27.2584 25.882V22.1196L24 20.2384L20.7417 22.1196Z"
+        stroke="#FDBA74"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
+
+function IconTwo() {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+      <path
+        d="M28.0413 20L23.9998 13L19.9585 20M32.0828 27.0001L36.1242 34H28.0415M19.9585 34H11.8755L15.9171 27"
+        stroke="#FB923C"
+        strokeWidth="2"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M18.804 30H29.1963L24.0001 21L18.804 30Z"
+        stroke="#FDBA74"
+        strokeWidth="2"
+      />
+    </svg>
+  );
+}
+
+function IconThree() {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
+      <rect x="13" y="32" width="2" height="4" fill="#FDBA74" />
+      <rect x="17" y="28" width="2" height="8" fill="#FDBA74" />
+      <rect x="21" y="24" width="2" height="12" fill="#FDBA74" />
+      <rect x="25" y="20" width="2" height="16" fill="#FDBA74" />
+      <rect x="29" y="16" width="2" height="20" fill="#FB923C" />
+      <rect x="33" y="12" width="2" height="24" fill="#FB923C" />
+    </svg>
+  );
+}
