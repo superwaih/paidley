62ea1995137ff7adc14c89c82fe9e6bded44e128
shittu adewale <sheriffhardewale@gmail.com>
2023-04-04 18:30:09 +0100

    started notifications page

--- components/dashboard/EmptyState.jsx
+++ components/dashboard/EmptyState.jsx
@@ -5,7 +5,7 @@ const EmptyState = ({text, Icon}) => {
   return (
     <div className=' flex flex-col justify-center items-center cursor-pointer py-12'>
        <div className='rounded-full bg-[#EAF0F6] p-4'>
-       <Icon />
+       <Icon className="text-[#ACC4E0] text-2xl" />
        </div>
         <p className='gray-text text-center text-lg'>{text}</p>
     </div>
--- pages/dashboard/notification/index.jsx
+++ pages/dashboard/notification/index.jsx
@@ -1,10 +1,23 @@
 import DashboardLayout from '@/components/dashboard/DashboardLayout'
+import EmptyState from '@/components/dashboard/EmptyState'
+import SimpleNav from '@/components/dashboard/SimpleNav'
 import React from 'react'
+import {AiOutlineBell} from "react-icons/ai"
 
 const NotificationsPage = () => {
   return (
     <DashboardLayout>
-        <div>NotificationsPage</div>
+      <SimpleNav 
+      title={"Notifications"}
+      />
+        <div  className='px-5 md:px-8'>
+          <div className='h-[50vh] flex justify-center items-center'>
+            <EmptyState 
+            text={"You have no notifications yet"}
+            Icon={AiOutlineBell}
+            />
+          </div>
+        </div>
 
     </DashboardLayout>
   )
