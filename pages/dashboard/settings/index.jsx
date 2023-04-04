a4a889af2ecc459f2e65b8005eda1c40ab245205
shittu adewale <sheriffhardewale@gmail.com>
2023-04-04 19:04:26 +0100

    settings page done

--- pages/dashboard/settings/index.jsx
+++ pages/dashboard/settings/index.jsx
@@ -1,6 +1,7 @@
 import DashboardLayout from '@/components/dashboard/DashboardLayout'
 import SimpleNav from '@/components/dashboard/SimpleNav'
 import React from 'react'
+import { AiFillBell, AiFillSetting } from 'react-icons/ai'
 
 const SettingsPage = () => {
   return (
@@ -8,8 +9,34 @@ const SettingsPage = () => {
       <SimpleNav 
       title={"Settings"}
       />
-      <div className='px-5 md:px-8 '>
-        
+      <div className='px-5 md:px-8 flex flex-col gap-4'>
+        <div className='border py-4 px-2 shadow-sm hover:border-brand-color hover:border rounded-md  cursor-pointer'>
+          <div className='flex gap-2 items-center'>
+            <AiFillBell className='text-2xl' />
+            <p>Notification Settings</p>
+          </div>
+        </div>
+
+        <div className='border py-4 px-2 hover:border-brand-color shadow-sm hover:border rounded-md  cursor-pointer'>
+          <div className='flex gap-2 items-center'>
+            <AiFillSetting className='text-2xl' />
+            <p>Settings</p>
+          </div>
+        </div>
+
+        <div className='border py-4 px-2 shadow-sm hover:border-brand-color hover:border rounded-md  cursor-pointer'>
+          <div className='flex gap-2 items-center'>
+            <AiFillBell className='text-2xl' />
+            <p>Notification Settings</p>
+          </div>
+        </div>
+
+        <div className='border py-4 px-2 hover:border-brand-color shadow-sm hover:border rounded-md  cursor-pointer'>
+          <div className='flex gap-2 items-center'>
+            <AiFillSetting className='text-2xl' />
+            <p>Settings</p>
+          </div>
+        </div>
       </div>
     </DashboardLayout>
   )
