587a71beaa0867f2b02892531d974ac9a0e6fd8f
shittu adewale <sheriffhardewale@gmail.com>
2023-04-05 07:36:32 +0100

    added profile page route

--- components/dashboard/notifications/SingleNotification.jsx
+++ components/dashboard/notifications/SingleNotification.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { BsArrowUpRight } from 'react-icons/bs'
 
 const SingleNotification = () => {
   return (
-    <div>
-        
+    <div className='flex w-full justify-between py-3'>
+       
+
+        <div className='flex gap-2'>
+        <div className='bg-[#E1FFE4] p-[10px] md:py-2 md:px-4 flex items-center rounded-full'>
+            <BsArrowUpRight className='text-[#0B1C3F] text-xl' />
+        </div>
+           <div>
+           <h3 className='font-semibold'>Your USD Card got Charged</h3>
+            <p className='text-[#9399A0]'>APPLE.COM/BILL www.apple.com IE</p>
+            <p className='text-[#9399A0]'>2 Sept 2023, 12:30 Am</p>
+           </div>
+        </div>
+
+        <div>
+            <h4>USD -3.34</h4>
+            <p className='text-[#37C546]'>Successful</p>
+
+        </div>
+
+
+
     </div>
   )
 }
--- pages/dashboard/notification/index.jsx
+++ pages/dashboard/notification/index.jsx
@@ -1,5 +1,6 @@
 import DashboardLayout from '@/components/dashboard/DashboardLayout'
 import EmptyState from '@/components/dashboard/EmptyState'
+import SingleNotification from '@/components/dashboard/notifications/SingleNotification'
 import SimpleNav from '@/components/dashboard/SimpleNav'
 import React from 'react'
 import {AiOutlineBell} from "react-icons/ai"
@@ -10,13 +11,18 @@ const NotificationsPage = () => {
       <SimpleNav 
       title={"Notifications"}
       />
-        <div  className='px-5 md:px-8'>
-          <div className='h-[50vh] flex justify-center items-center'>
+        <div  className='px-5 md:px-8 flex gap-4 flex-col w-full max-w-3xl'>
+          {/* <div className='h-[50vh] flex justify-center items-center'>
             <EmptyState 
             text={"You have no notifications yet"}
             Icon={AiOutlineBell}
             />
-          </div>
+          </div> */}
+          <SingleNotification />
+          <SingleNotification />
+          <SingleNotification />
+          <SingleNotification />
+
         </div>
 
     </DashboardLayout>
