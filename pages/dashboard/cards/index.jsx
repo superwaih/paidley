b0b5a294ca5e33617d0a70c235e3cce726716d16
shittu adewale <sheriffhardewale@gmail.com>
2023-04-08 11:26:17 +0100

    added virtual card

--- pages/dashboard/cards/index.jsx
+++ pages/dashboard/cards/index.jsx
@@ -4,7 +4,11 @@ import { PhysicalCard } from '@/public/svgs/physical-card'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 import React, { useState } from 'react'
+import CardVirtual from "../../../public/images/virtual-card.png"
 import CardSample from "../../../public/images/card-sample.png"
+import { AiOutlineEye } from 'react-icons/ai'
+import {BsCreditCard2Back} from "react-icons/bs"
+import {RxCaretRight} from "react-icons/rx"
 const CardsPage = () => {
   const router = useRouter()
   const[cardType, setCardType] = useState("physical")
@@ -16,32 +20,83 @@ const CardsPage = () => {
           <button className=' rounded-[12px] border text-brand-color hover:bg-brand-color hover:text-white duration-300 border-brand-color px-4 py-2'>Back</button>
           </div>
 
-          <div className='flex px-8 justify-center items-center  w-full'>
-          <div className='flex mt-[10px] md:mt-[90px]  rounded-md border  hover:[#F3F7FA] bg-white px-6 py-4 flex-col space-y-6 rouned-md shadow-md max-w-lg'>
+          <div className='flex px-8 justify-center duration-300 items-center  w-full'>
+          <div className='flex mt-[10px] md:mt-[90px] duration-300  rounded-md border  hover:[#F3F7FA] bg-white px-6 py-4 flex-col space-y-6 rouned-md shadow-md max-w-lg'>
            <div className=' justify-between w-full'>
-           <div className=' md:px-16 duration-300 gap-4 flex'>
+           <div className='justify-center items-center md:px-16 duration-300 gap-4 flex'>
            <button onClick={() => setCardType("physical")}  className={cardType === "physical" ? 'border-brand-color border-b-2' : 'gray-text'}>Physical Card</button>
             <button onClick={() => setCardType("virtual")} className={cardType === "virtual" ? 'border-brand-color border-b-2' : 'gray-text'}>Virtual Card</button>
            </div>
 
            </div>
             {/* <PhysicalCard /> */}
-            <Image
+           {
+            cardType === "physical" &&(
+              <div className='flex items-center justify-center'>
+                <Image
               width={0}
               height={0}
-              objectFit="cover"
               src={CardSample}
               alt=""
             />
+              </div>
+            )
+           }
+             {
+            cardType === "virtual" &&(
+             <div className='flex items-center space-y-4 flex-col justify-center
+             '>
+               <Image
+              width={0}
+              height={0}
+              src={CardVirtual}
+              alt=""
+            />
+            <button className='bg-[#E8F7FD] gap-2  text-brand-color p-3 flex
+             items-center'> <AiOutlineEye /> Show card details</button>
+              </div>
+            )
+           }
             {/* <h3 className='font-bold  text-xl'>Send Money</h3>
             <p className='gray-text '>Select Transfer Method</p> */}
 
 
             <div className='flex flex-col gap-4'>
-                    <button onClick={() => router.push("/dashboard/transfer/send-money")} className='btn-full rounded-[12px] duration-300'>Proceed</button>
-                    <button 
-                    onClick={() => router.back()}
-                    className='text-[#306BB1] flex w-full justify-center font-semibold items-center text-center'>Cancel and go back</button>
+                   {
+                    cardType === "physical" && (
+                      <>
+                      <button onClick={() => router.push("/dashboard/transfer/send-money")} className='btn-full rounded-[12px] duration-300'>Proceed</button>
+                      <button 
+                      onClick={() => router.back()}
+                      className='text-[#306BB1] flex w-full justify-center font-semibold items-center text-center'>Cancel and go back</button>
+          
+                      </>
+                    )
+                   }
+
+{
+                    cardType === "virtual" && (
+                      <div className='bg-[#F3F7FA] rounded-md cursor-pointer py-3 px-2 flex justify-between border-[#306BB2] border '>
+                        <div className='flex gap-3 items-center'>
+                        <div className='rounded-full bg-[#EAF0F6] p-3'>
+       <BsCreditCard2Back className="text-[#ACC4E0] text-2xl" />
+       </div>
+                       
+                          <div>
+                            <h3 className='font-semibold text-brand-color'>Manage Card</h3>
+                            <p className='gray-text'>Update your card info.</p>
+                          </div>
+
+                          <div className='flex justify-end items-end'>
+                          <RxCaretRight className='text-brand-color text-2xl' />
+                          </div>
+                        </div>
+
+                    
+          
+                      </div>
+                    )
+                   }
               </div>
           </div>
         </div>
